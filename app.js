a16262d12b92c09e693075e5ad78fe0cd8f151c5
Henry <dokinruss@gmail.com>
2022-05-14 12:48:04 +0000

    make the card session dynamic

--- app.js
+++ app.js
@@ -1,3 +1,171 @@
+
+const cardObject = cardDetails;
+const mainWindow = document.querySelector('.mainWindow');
+const popupCard = document.querySelector('.popupContainer');
+const cardContainer = document.querySelector('.card-container');
+const workDetails = document.querySelector('.worklist');
+  
+
+
+  
+const popupmethod = (projectId) => {
+    let carditems = {};
+
+// filter 
+for (let i = 0; i < cardObject.length; i += 1) {
+    if (cardObject[i].id === projectId) {
+      carditems = { ...cardObject[i] };
+    }
+  }
+
+ 
+    
+  const popupImg = document.querySelector('.card-image')
+    
+  const projectTitle = document.querySelector('.Popup-project-title');
+  const popuDetail = document.querySelector('.popup-details-sec');
+  const technList = document.createElement('ul');
+  popupImg.setAttribute('src', carditems.featuredImage);
+  projectTitle.innerText = carditems.projectName;
+  popuDetail.innerText = carditems.projectDescription;
+
+  
+
+     carditems.works.forEach((elem) => {
+         const item = document.createElement('li');
+         // item.classList.add('tag-text');
+         item.classList.add('workInfoDetails');
+         item.classList.add('workInfoDetailsLink');
+        
+         item.classList.add('workInfo');
+      item.innerText = elem;
+        technList.append(item);
+      });
+      
+       workDetails.replaceChildren(technList);
+
+}
+
+
+
+
+// see project handler
+const buttonMethod = (event) => {
+    const id = parseInt(event.target.id, 10);
+    popupmethod(id);
+    
+        // mainWindow.style.display = 'none'; 
+    popupCard.style.display =  'block';
+    
+  }
+
+  // console.log(popupCard.classList)
+
+
+
+
+  // project card builder
+const projectCard = () => {
+    cardObject.forEach((data) => {
+      const card = document.createElement('div');
+      const cardContentArea = document.createElement('div');
+      const imageContainer = document.createElement('div');
+
+      const featuredImg = document.createElement('img');
+      const projectTitle = document.createElement('h2');
+       const technologiesDetails = document.createElement('ul');
+      const workDetails = document.createElement('ul');
+      
+      const projectDescription = document.createElement('p');
+      const Btn = document.createElement('button');
+      cardContentArea.classList.add('card-content-area');
+      imageContainer.classList.add('card-img-head')
+
+      technologiesDetails.classList.add('tag-btn-container')
+  
+      card.classList.add('card');
+      featuredImg.classList.add('card-img');
+      featuredImg.classList.add('card-img');
+      projectTitle.classList.add('project-title');
+      workDetails.classList.add('workInfo');
+      workDetails.classList.add('workInfoDetails');
+      Btn.classList.add('btnNormal');
+      projectDescription.classList.add('details-sec')
+  
+      featuredImg.setAttribute('src', data.featuredImage);
+      projectTitle.innerText = data.projectName;
+      projectDescription.innerHTML = data.projectDescription;
+
+      Btn.innerText = 'See Project';
+      Btn.setAttribute('id', data.id);
+    
+      
+      Btn.addEventListener('click', buttonMethod);
+
+      data.works.forEach((item) => {
+        const workItem = document.createElement('a');
+        workItem.classList.add('workInfoDetails');
+        workItem.classList.add('workInfoDetailsLink');
+       
+        workItem.classList.add('workInfo');
+        workItem.innerText = item;
+        workDetails.append( workItem);
+      });
+
+      
+
+      data.technologies.forEach((item) => {
+        const technoItem = document.createElement('button');
+        technoItem.classList.add('tag-btn-container');
+        technoItem.classList.add('tag-btn');
+        technoItem.classList.add('btn-small');
+        technoItem.innerText = item;
+        technologiesDetails.append( technoItem);
+      });
+
+      card.append(imageContainer);
+
+      imageContainer.append(featuredImg)
+        
+       cardContentArea.append(projectTitle,  workDetails, projectDescription, technologiesDetails ,  Btn)
+      card.append(cardContentArea);
+      cardContainer.append(card);
+    });
+  };
+
+  window.onload = () => {
+    projectCard();
+  };
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// navigation script
 const navOpenBtn = document.querySelector("#btnOpen");
 const navCloseBtn = document.querySelector("#btnClose");
 const navElem = document.querySelector("nav");
---
+++ objectss.js
@@ -0,0 +1,38 @@
+const cardDetails = [
+    {
+      id: 1,
+      projectName: 'Tonic',
+      projectDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. ',
+      featuredImage: 'https://henrycode460.github.io/Portfolio-Mobile-Version-Skeleton/desk-v-card-one.svg',
+      technologies: ['html', 'CSS', 'JavaScript'],
+      works: ['Canopy', 'Back End',  '2015'],
+      
+    },
+    {
+      id: 2,
+      projectName: 'Multi-Post Stories',
+      projectDescription: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      featuredImage: 'https://henrycode460.github.io/Portfolio-Mobile-Version-Skeleton/img-two-desktop.svg',
+      technologies: [ 'JavaScript', 'HTML', 'CSS', ],
+      works: ['Facebook', 'Full Stack', '2015'],
+    },
+    {
+      id: 3,
+      projectName: 'Facebook 360',
+      projectDescription: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR',
+      featuredImage: 'https://henrycode460.github.io/Portfolio-Mobile-Version-Skeleton/card-three-desktop.svg',
+      technologies: ['html', 'css', 'JavaScript'],
+      works: ['Facebook', 'Full Stack', '2015'],
+      
+    },
+    {
+      id: 4,
+      projectName: 'Uber Navigation',
+      projectDescription: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+      featuredImage: 'https://henrycode460.github.io/Portfolio-Mobile-Version-Skeleton/card-four-desktop.svg',
+      technologies:  ['html', 'css', 'JavaScript'],
+      works: ['Facebook', 'Full Stack', '2015'],
+    }
+   
+  ];
+  
