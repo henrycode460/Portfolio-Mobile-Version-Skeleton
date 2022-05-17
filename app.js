9dbd6f41e6c5e397ddac375e2d8a8421f7ce2aaf
Henry <dokinruss@gmail.com>
2022-05-17 23:20:28 +0000

    Add style to navbar to suite fffigma template

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-
 const cardObject = cardDetails;
 const mainWindow = document.querySelector('.mainWindow');
 const popupCard = document.querySelector('.popupContainer');
 const cardContainer = document.querySelector('.card-container');
 const workDetails = document.querySelector('.worklist');
 const techDetails = document.querySelector('.technologiesContainer');
-  
 
-
-  
+// popup function
 const popupmethod = (projectId) => {
     let carditems = {};
     
@@ -21,9 +18,7 @@ for (let i = 0; i < cardObject.length; i += 1) {
   }
 
  
-    
   const popupImg = document.querySelector('.card-image')
-    
   const projectTitle = document.querySelector('.Popup-project-title');
   const popuDetail = document.querySelector('.popup-details-sec');
   const technList = document.createElement('ul');
@@ -54,29 +49,18 @@ for (let i = 0; i < cardObject.length; i += 1) {
      
       techDetails.replaceChildren(technologiesList);
 
-
-
-
 }
-
-
 
 
 // see project handler
 const buttonMethod = (event) => {
     const id = parseInt(event.target.id, 10);
     popupmethod(id);
-    
-        // mainWindow.style.display = 'none'; 
     popupCard.style.display =  'block';
     
   }
 
-  // console.log(popupCard.classList)
-
-
-
-
+ 
   // project card builder
 const projectCard = () => {
     cardObject.forEach((data) => {
@@ -108,25 +92,20 @@ const projectCard = () => {
       featuredImg.setAttribute('src', data.featuredImage);
       projectTitle.innerText = data.projectName;
       projectDescription.innerHTML = data.projectDescription;
-
       Btn.innerText = 'See Project';
       Btn.setAttribute('id', data.id);
-    
-      
       Btn.addEventListener('click', buttonMethod);
 
       data.works.forEach((item) => {
         const workItem = document.createElement('a');
         workItem.classList.add('workInfoDetails');
         workItem.classList.add('workInfoDetailsLink');
-       
         workItem.classList.add('workInfo');
         workItem.innerText = item;
         workDetails.append( workItem);
       });
 
-      
-
+     
       data.technologies.forEach((item) => {
         const technoItem = document.createElement('button');
         technoItem.classList.add('tag-btn-container');
@@ -151,8 +130,6 @@ const projectCard = () => {
   };
   
 
-
-
 // close modal box when user click anywhere outside the modal
 window.onclick = (event) => {
   if (event.target ===  popupCard) {
@@ -169,37 +146,11 @@ const closePopupBox = () => {
  const closePopup = document.querySelector('.close-popup');
  closePopup.addEventListener('click', closePopupBox);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // navigation script
 const navOpenBtn = document.querySelector("#btnOpen");
 const navCloseBtn = document.querySelector("#btnClose");
 const navElem = document.querySelector("nav");
 let closeNavBar = document.querySelectorAll(".navbarTwo-link");
-
-
-
 
 for(i = 0; i < closeNavBar.length; i++ ){
     closeNavBar[i].addEventListener('click', function (){
@@ -220,7 +171,3 @@ navCloseBtn.addEventListener("click", function(){
     document.getElementById("btnOpen").style.visibility = "visible";
 
 })
-
-
-
-
